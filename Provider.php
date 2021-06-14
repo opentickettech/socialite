5a50c131feaa0c713013ce9cb29c64ac643ae6bc
Thomas <thomas@vzadelhoff.nl>
2021-06-14 16:52:11 +0200

    Implements mapUserToObject method
    Signed-off-by: Thomas <thomas@vzadelhoff.nl>

--- Provider.php
+++ Provider.php
@@ -4,12 +4,15 @@ namespace Opentickettech\Socialite;
 
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
+use SocialiteProviders\Manager\Contracts\OAuth2\ProviderInterface;
+use Illuminate\Support\Arr;
 
-class Provider extends AbstractProvider
+class Provider extends AbstractProvider implements ProviderInterface
 {
     const IDENTIFIER = "OPENTICKETTECH";
 
-    public function getAccessTokenRefreshResponse ($refreshToken) {
+    public function getAccessTokenRefreshResponse($refreshToken)
+    {
         $response = $this->getHttpClient()->post($this->getTokenUrl(), [
             "headers"     => ["Accept" => "application/json"],
             "form_params" => $this->getTokenRefreshFields($refreshToken),
@@ -18,23 +21,28 @@ class Provider extends AbstractProvider
         return json_decode($response->getBody(), true);
     }
 
-    protected function getAuthUrl ($state) {
+    protected function getAuthUrl($state)
+    {
         return $this->buildAuthUrlFromBase("https://auth.openticket.tech/token/authorize", $state);
     }
 
-    protected function getTokenUrl () {
+    protected function getTokenUrl()
+    {
         return "https://auth.openticket.tech/token";
     }
 
-    public function userFromToken($token) {
+    public function userFromToken($token)
+    {
         return $this->getUserByToken($token);
     }
 
-    protected function getUserByToken ($token) {
+    protected function getUserByToken($token)
+    {
         $userUrl = "https://auth.openticket.tech/user/me";
 
         $response = $this->getHttpClient()->get(
-            $userUrl, $this->getRequestOptions($token)
+            $userUrl,
+            $this->getRequestOptions($token)
         );
 
         $user = json_decode($response->getBody(), true);
@@ -42,18 +50,25 @@ class Provider extends AbstractProvider
         return $user;
     }
 
-    protected function mapUserToObject (array $user) {
-        // TODO - Implement
-        throw new \BadMethodCallException("This function has not been implemented yet");
+    protected function mapUserToObject(array $user)
+    {
+        return (new User)->setRaw($user)->map([
+            'id' => Arr::get($user, 'guid'),
+            'nickname' => Arr::get($user, 'email'),
+            'name' => Arr::get($user, 'name'),
+            'email' => Arr::get($user, 'email'),
+        ]);
     }
 
-    protected function getTokenFields ($code) {
+    protected function getTokenFields($code)
+    {
         return array_merge(parent::getTokenFields($code), [
             "grant_type" => "authorization_code",
         ]);
     }
 
-    protected function getTokenRefreshFields ($refreshToken) {
+    protected function getTokenRefreshFields($refreshToken)
+    {
         return [
             "client_id"     => $this->clientId,
             "client_secret" => $this->clientSecret,
@@ -73,7 +88,7 @@ class Provider extends AbstractProvider
         return [
             'headers' => [
                 'Accept' => 'application/json',
-                'Authorization' => 'Bearer '.$token,
+                'Authorization' => 'Bearer ' . $token,
             ],
         ];
     }
